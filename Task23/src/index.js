abdaccf7a6067b107340c06e4fcd21f5ef4a1a72
Medam Indra Sena Reddy <110720455+indra-8309@users.noreply.github.com>
2023-05-20 17:25:01 +0530

    Add files via upload

---
+++ Task23/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from '../src/components/App/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+    <App />
+ 
+);
+
+
